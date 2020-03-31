2f13c1ac22118805f92cc82ffca49886444864be
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-03-31 10:42:02 +0530

    Revert commit for key files.

--- src/routers/tournamentRouter.js
+++ src/routers/tournamentRouter.js
@@ -6,22 +6,22 @@ const router = express.Router();
 /**
  * View to add a tournament
  */
-router.get('/tournament/add',()=>{
-    res.render('tournaments/tournament_create')
-})
+router.get('/tournament/add', () => {
+   res.render('tournaments/tournament_create')
+});
 
 /**
  * Store a new tournament
  */
 router.post('/tournament/add', async (req, res) => {
-    res.send('create tournament route');
+   res.send('create tournament route');
 });
 
 /**
  * Edit a tournament
  */
 router.patch('/tournament/edit', async (req, res) => {
-    res.send('Edit tournament route');
+   res.send('Edit tournament route');
 });
 
 module.exports = router;
