d7bb77fd9b65b646d2fde99c9a399eb03771a4c9
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-04-03 12:43:14 +0530

    Added players add in tournament.

--- src/models/Tournament.js
+++ src/models/Tournament.js
@@ -8,16 +8,19 @@ class Tournament {
     * _localization
     * _maxPlayers
     * _buyIn
+    * _tournamentID
     * @param creator
     * @param localization
     * @param maxPlayers
     * @param buyIn
+    * @param tournamentID
     */
-   constructor(creator, localization, maxPlayers, buyIn) {
+   constructor(creator, localization, maxPlayers, buyIn, tournamentID) {
       this._creator = validators.validateString(creator) ? creator : false;
       this._localization = validators.validateString(localization) ? localization : false;
       this._maxPlayers = validators.validateNumber(maxPlayers) ? maxPlayers : false;
       this._buyIn = validators.validateNumber(buyIn) ? buyIn : false;
+      this._tournamentID = validators.validateString(tournamentID) ? tournamentID : false;
    }
 
    /**
@@ -35,11 +38,35 @@ class Tournament {
          tournamentObj[constants.TOURNAMENT_BUY_IN] = this._buyIn;
          tournamentObj[constants.TOURNAMENT_DESCRIPTION] = description;
          tournamentObj[constants.TOURNAMENT_COMPLETED] = false;
-         
+
          database().then(db => {
             db.collection(constants.TOURNAMENTS_COLLECTION).insertOne(tournamentObj).then(_resultSet => {
                console.log("Tournament created: " + _resultSet.insertedId);
                resolve(_resultSet.insertedId);
+            });
+         }).catch(err => {
+            console.error(err);
+            reject(err);
+         });
+      });
+   }
+
+   /**
+    * Adding players in tournament.
+    * @param playersId: The players id.
+    * @returns {Promise<unknown>}
+    */
+   addUsers(playersId) {
+      return new Promise((resolve, reject) => {
+         const filterObj = {};
+         filterObj._id = this._tournamentID;
+         const setObj = {};
+         setObj[constants.TOURNAMENT_PLAYERS] = {id: playersId, rank: 0};
+         const setFinalObj = {};
+         setFinalObj[constants.PUSH_OPERATOR] = setObj;
+         database().then(db => {
+            db.collection(constants.TOURNAMENTS_COLLECTION).update(filterObj, setFinalObj).then(_resultSet => {
+               console.log(_resultSet);
             });
          }).catch(err => {
             console.error(err);
