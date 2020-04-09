0a44428f1f534fb69657d27b8dde56e428acffc9
Matthieu MARTIN <codingstuff91@gmail.com>
2020-04-09 12:34:18 +0200

    Edit expiration delay of the JWT. Refactoring of tokenGenerator

--- src/utils/tokenGenerator.js
+++ src/utils/tokenGenerator.js
@@ -3,22 +3,24 @@ const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const path = require('path');
 const tokenGenerator = {};
+
 /**
- * * Method to generate the JwToken.
+ * * Generate the JwToken.
  * @param data: The data to be converted to the JwToken.
  * @returns {null|String}: The JwToken if generated else NULL.
  */
 tokenGenerator.generateToken = (data) => {
    try {
       const keyFile = fs.readFileSync(path.resolve(__dirname + "/../keyFiles/first.pem"), 'utf8');
-      return jwt.sign(data, keyFile, {algorithm: 'RS256', expiresIn: '8h'}).toString();
+      return jwt.sign(data, keyFile, {algorithm: 'RS256', expiresIn: '7d'}).toString();
    } catch (e) {
       console.error(e);
       return null;
    }
 };
+
 /**
- * Method to validate the Token.
+ * Validate the Token.
  * @param token: The token to be validated.
  * @returns {boolean|Object}: The data encrypted in the token else false if invalid.
  */
@@ -31,6 +33,7 @@ tokenGenerator.validateToken = (token) => {
       return false;
    }
 };
+
 /**
  * Exporting the token.
  * @type {{}}
